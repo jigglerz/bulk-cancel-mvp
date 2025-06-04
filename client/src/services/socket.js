907b8a42db6f95e6e003631eb4e4fe354365daf9
jigglerz <61538027+jigglerz@users.noreply.github.com>
2025-06-04 17:30:07 +0300

    Update socket.js

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,7 +1,11 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000', {
-  transports: ['websocket']
+// Replace localhost with your actual server public IP or domain (since this runs in the browser)
+const socket = io('http://13.60.204.82:3000', {
+  transports: ['websocket'],
 });
+
+// Expose to window for debugging
+window.socket = socket;
 
 export default socket;
